25914626a8befa8b6d03c466c4a09f6be12b82e1
Valerie Foster <fosterv2@outlook.com>
2019-11-08 07:03:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function increaseRankBy(n) {
     .getElementById('app')
     .querySelectorAll('ul.ranked-list li')
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i] innerHTML, 10) + n
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + n
   }
 }
 
